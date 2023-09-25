0415fa068d0cb56bfe7a0aecddbf5be30a14ecf1
neewamp <neewamp@gmail.com>
2023-09-25 00:29:58 -0400

    Checking for scenarios where a bad action could happen given two excel sheets of controls actions and hazards

--- model_helpers.py
+++ model_helpers.py
@@ -5,8 +5,8 @@ from z3 import *
 # import pandas as pd
 
 def prove_solver(self, f):
-    self.add(Not(f))
-    if self.check() == unsat:
+    #self.add(Not(f))
+    if self.check(Not(f)) == unsat:
         print(f, ", universally holds", sep='')
     else:
         print(f, ', does not universally hold here is a counter example: ', sep='')
@@ -31,7 +31,10 @@ def create_model(sheet):
     df = ipy.to_dataframe(sheet)
     variables = {}
     s = Solver()
-    model_vars = df.columns[:-1]
+    variables, action_variables = s.append_to_model(sheet)
+    return s, variables, action_variables
+
+"""     model_vars = df.columns[:-1]
     name_to_index = {name: i+1 for i,name in enumerate(model_vars)}
 
     name_to_index[df.columns[-1]] = len(df.columns)
@@ -59,3 +62,37 @@ def create_model(sheet):
                 actions.append(Not(action_variables[action]))
         s.add(Implies(And(conjuncts), And(actions)))
     return s, variables, action_variables
+ """
+def append_to_model(self, sheet): 
+    df = ipy.to_dataframe(sheet)
+    variables = {}
+    model_vars = df.columns[:-1]
+    name_to_index = {name: i+1 for i,name in enumerate(model_vars)}
+
+    name_to_index[df.columns[-1]] = len(df.columns)
+
+    for value in model_vars:
+        variables[value] = Bool(value)
+
+
+    action_variables = {}
+
+    for row in df.itertuples():
+        for action in row.Control_Action.split(' '):
+            if action not in action_variables.keys() and action != '':
+                action_variables[action] = Bool(action)
+
+    for row in df.itertuples():
+        conjuncts = [variables[pvar] if row[name_to_index[pvar]].lower() == "true" else Not(variables[pvar]) \
+                      for pvar in model_vars]
+
+        actions = []
+        for action in action_variables.keys():
+            if action in row.Control_Action.split(' '):
+                actions.append(action_variables[action])
+            else:
+                actions.append(Not(action_variables[action]))
+        self.add(Implies(And(conjuncts), And(actions)))
+    return variables, action_variables
+
+Solver.append_to_model = append_to_model
